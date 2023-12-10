bb2f275ff8def1e2482999ba2ca75df8f18ea544
Denys Hyde <denys.hyde@kcl.ac.uk>
2023-12-10 21:53:27 +0000

    updated day 6 code

--- day-6-see-the-rgb.py
+++ day-6-see-the-rgb.py
@@ -7,6 +7,38 @@ from neopixel import NeoPixel
 GRBled1 = NeoPixel(Pin(2), 1)
 GRBled2 = NeoPixel(Pin(5), 1)
 
+# Activity 1: Multiple colour examples
+# Define some GRB colour variables
+white = 240,140,255 #White-ish!
+red = 0,255,0
+green = 255,0,0
+blue = 0,0,255
+yellow = 255,175,150
+orange = 238, 223, 105
+pink = 150,150,200
+purple = 40,100,255
+iceblue = 150,25,200
+unicorn = 175,150,255
+bogey = 215, 100, 0
+
+GRBled1.fill((purple))
+GRBled1.write()
+
+# Activity 2: Colour cycling
+# Define our colour list
+colours = [white, red, green, blue, yellow, orange, pink, purple, iceblue, unicorn, bogey]
+
+while True:
+    
+    for colour in colours:
+        
+        GRBled.fill((colour))
+        
+        GRBled.write()
+        
+        time.sleep(0.1)
+
+# Activity 3: GRB value change and light passing
 while True:
     
     # First LED fades in
